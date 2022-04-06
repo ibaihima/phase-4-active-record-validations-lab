818e7b9a374a363a4dcde79c24e0351b1b99f64f
Ibaihima <ibaihima@hotmail.com>
2022-04-06 13:14:49 -0400

    commit tree 95cf2825f240aae30d14af577bfba78b698fbbc8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :title_is_clickbait
+    
+
+    def title_is_clickbait
+        if title != nil
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.include?("Guess")
+                errors.add(:title, "tittle is not click bait")
+            end
+        end
+    end
 end
